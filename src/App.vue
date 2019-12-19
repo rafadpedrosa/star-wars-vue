25eef849edfcf1a13b9798d703c1f295dd8aaeff
Rafael Dias Pedrosa <rafadpedrosa@gmail.com>
2019-12-19 17:20:50 -0300

    Add icon and an animation for the darth vader

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,60 @@
 <template>
-  <div id="app">
-    <b-navbar toggleable="lg" type="dark" variant="info">
-      <b-navbar-brand href="#">NavBar</b-navbar-brand>
+    <div id="app">
+        <b-navbar toggleable="lg" type="dark" variant="dark">
+            <b-navbar-brand href="#">
+                <img src="../src/assets/icon-sw.png" alt="piuu piu" width="50">
+            </b-navbar-brand>
 
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-      <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav>
-          <b-nav-item to="/Home" href="#">Home</b-nav-item>
-          <b-nav-item to="/People" href="#">People</b-nav-item>
-          <b-nav-item to="/Films" href="#">Films</b-nav-item>
-          <b-nav-item to="/Starships" href="#">Starships</b-nav-item>
-          <b-nav-item to="/Vehicles" href="#">Vehicles</b-nav-item>
-          <b-nav-item to="/Species" href="#">Species</b-nav-item>
-          <b-nav-item to="/Planets" href="#">Planets</b-nav-item>
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
-    <router-view/>
-  </div>
+            <b-collapse id="nav-collapse" is-nav>
+                <b-navbar-nav>
+                    <b-nav-item :active="isActive('/Home')" href="#" to="/Home">Home</b-nav-item>
+                    <b-nav-item :active="isActive('/People')" href="#" to="/People">People</b-nav-item>
+                    <b-nav-item :active="isActive('/Films')" href="#" to="/Films">Films</b-nav-item>
+                    <b-nav-item :active="isActive('/Starships')" href="#" to="/Starships">Starships</b-nav-item>
+                    <b-nav-item :active="isActive('/Vehicles')" href="#" to="/Vehicles">Vehicles</b-nav-item>
+                    <b-nav-item :active="isActive('/Species')" href="#" to="/Species">Species</b-nav-item>
+                    <b-nav-item :active="isActive('/Planets')" href="#" to="/Planets">Planets</b-nav-item>
+                </b-navbar-nav>
+            </b-collapse>
+        </b-navbar>
+        <router-view/>
+    </div>
 </template>
 
-<style lang="scss">
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-  }
-
-  #nav {
-    padding: 30px;
-
-    a {
-      font-weight: bold;
-      color: #2c3e50;
-
-      &.router-link-exact-active {
-        color: #42b983;
-      }
+<script>
+export default {
+  name: 'app',
+  methods: {
+    isActive (path) {
+      // eslint-disable-next-line no-debugger
+      debugger
+      return this.$route.path === path
     }
   }
+}
+</script>
+
+<style lang="scss">
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+    }
+
+    #nav {
+        padding: 30px;
+
+        a {
+            font-weight: bold;
+            color: #2c3e50;
+
+            &.router-link-exact-active {
+                color: #42b983;
+            }
+        }
+    }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,6 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="pt-4">
+    <HelloWorld class="row justify-content-center" msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
@@ -16,3 +15,8 @@ export default {
   }
 }
 </script>
+
+<style>
+  .home {
+  }
+</style>
