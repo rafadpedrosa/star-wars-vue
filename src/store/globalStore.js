865ce4fb92f67bf40eabb39d9a39e6f0d878dd1d
Rafael Dias Pedrosa <rafadpedrosa@gmail.com>
2019-12-19 17:36:30 -0300

    Add VUEX stores

---
+++ src/store/globalStore.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import peopleStore from './modules/peopleStore.js'
+import filmsStore from './modules/filmsStore.js'
+import planetsStore from './modules/planetsStore.js'
+import speciesStore from './modules/speciesStore.js'
+import vehiclesStore from './modules/vehiclesStore.js'
+import starshipStore from './modules/starshipStore.js'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  modules: {
+    peopleStore,
+    filmsStore,
+    planetsStore,
+    speciesStore,
+    vehiclesStore,
+    starshipStore
+  },
+  state: {},
+  mutations: {},
+  actions: {}
+})
